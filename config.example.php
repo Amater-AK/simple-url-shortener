c5565d56e5accfcbe05cb896ddedc19cf50c0124
Andrey Kupetskov <amater131089@gmail.com>
2022-07-16 14:43:49 +0300

    Added basic functionality (very basic)

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    "database" : [
+        "dbname" : "-test.demo-",
+        "host" : "127.0.0.1",
+        "charset" : "utf8mb4",
+        "user" : "-test-",
+        "pass" : "-test-"
+    ]
+];
